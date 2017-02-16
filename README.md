87a84e8c13e658a4def3e4c726adf46d622e8f4e
Samuel Marks <samuelmarks@gmail.com>
2017-02-16 13:44:21 +1100

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+glaucoma-risk-calculator REST API
+=================================
+
+REST API backend written on Node.JS in TypeScript with restify and waterline
+
+
+## Install prerequisites
+
+  0. node & npm (tested with node v7.5 and npm v4.2.0 on Ubuntu 16.10 x64)
+  1. Run: `npm install -g tsc typings`
+  2. `cd` to directory you've cloned this repo into
+  3. Run: `typings install`
+  4. Run: `npm install`
+
+## Compile+run app
+
+    tsc
+    node main.js
+
+Or:
+
+    npm start
---
+++ main.ts
@@ -0,0 +1,85 @@
+import * as redis from 'redis';
+import { Collection, Connection } from 'waterline';
+import * as waterline_postgres from 'waterline-postgresql';
+import { createLogger } from 'bunyan';
+import { Server } from 'restify';
+import { uri_to_config, populateModelRoutes, IModelRoute } from 'nodejs-utils';
+import { strapFramework, IStrapFramework } from 'restify-utils';
+import { SampleData } from './test/SampleData';
+
+export const package_ = require('./package');
+export const logger = createLogger({
+    name: 'main'
+});
+
+process.env['NO_DEBUG'] || logger.info(Object.keys(process.env).sort().map(k => ({[k]: process.env[k]})));
+
+export interface IObjectCtor extends ObjectConstructor {
+    assign(target: any, ...sources: any[]): any;
+}
+
+declare const Object: IObjectCtor;
+
+// Database waterline_config
+const db_uri: string = process.env['RDBMS_URI'] || process.env['DATABASE_URL'] || process.env['POSTGRES_URL'];
+
+
+export const waterline_config = Object.freeze({
+    /* TODO: Put this all in tiered environment-variable powered .json file */
+    adapters: {
+        url: db_uri,
+        postgres: waterline_postgres
+    },
+    defaults: {
+        migrate: 'create'
+    },
+    connections: {
+        postgres: {
+            adapter: 'postgres',
+            connection: uri_to_config(db_uri),
+            pool: {
+                min: 2,
+                max: 20
+            }
+        }
+    }
+});
+
+export const all_models_and_routes: IModelRoute = populateModelRoutes('.');
+
+export const redis_cursors: {redis: redis.RedisClient} = {
+    redis: null
+};
+
+export const c: {collections: Collection[], connections: Connection[]} = {collections: [], connections: []};
+
+let _cache = {};
+
+export const strapFrameworkKwargs: IStrapFramework = Object.freeze(<IStrapFramework>{
+    app_name: package_.name,
+    models_and_routes: all_models_and_routes,
+    logger: logger,
+    _cache: _cache,
+    package_: package_,
+    root: '/api',
+    skip_db: false,
+    collections: c.collections,
+    waterline_config: waterline_config,
+    use_redis: true,
+    redis_cursors: redis_cursors,
+    createSampleData: true,
+    SampleData: SampleData,
+    sampleDataToCreate: (sampleData: any) => [
+        cb => sampleData.unregister(cb),
+        cb => sampleData.registerLogin(cb)
+    ]
+});
+
+if (require.main === module) {
+    strapFramework(Object.assign({
+        start_app: true, callback: (err, _app: Server, _connections: Connection[], _collections: Collection[]) => {
+            if (err) throw err;
+            c.collections = _collections
+        }
+    }, strapFrameworkKwargs));
+}
